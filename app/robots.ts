309af86eb445eda7d74b2b4618b1f1e482bed7bf
michaelsalton <msalton6@gmail.com>
2025-10-21 15:09:27 -0400

    Add robots.txt to help with SEO indexing

--- app/robots.ts
+++ app/robots.ts
@@ -2,26 +2,10 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/api/', '/admin/'],
-        crawlDelay: 1,
-      },
-      // Optimize for major search engines
-      {
-        userAgent: ['Googlebot', 'Googlebot-Image'],
-        allow: '/',
-        disallow: ['/api/'],
-      },
-      {
-        userAgent: ['Bingbot', 'msnbot'],
-        allow: '/',
-        disallow: ['/api/'],
-      },
-    ],
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
     sitemap: 'https://saltboxinteractive.com/sitemap.xml',
-    host: 'https://saltboxinteractive.com',
   }
 }
