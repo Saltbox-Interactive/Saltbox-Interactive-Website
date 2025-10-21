e5364cbecc9b88424277675a09686b28d47b71af
michaelsalton <msalton6@gmail.com>
2025-10-21 08:01:36 -0400

    formatting subscribe section

--- components/sections/EmailSubscribe.tsx
+++ components/sections/EmailSubscribe.tsx
@@ -5,11 +5,11 @@ import BracketLink from "@/components/ui/BracketLink";
 
 export default function EmailSubscribe() {
   return (
-    <section className="relative bg-black py-16 sm:py-20 md:py-24 border-b border-accent/10">
+    <section className="relative bg-black py-16 sm:py-20 md:py-24 pb-36 sm:pb-44 md:pb-52 border-b border-accent/10">
       <div className="container mx-auto px-4 sm:px-6">
-        <div className="grid grid-cols-1 lg:grid-cols-[auto_1fr_1fr] gap-8 lg:gap-12 items-start lg:items-center">
+        <div className="relative flex flex-col lg:flex-row lg:items-start gap-8">
           {/* Left - Logo */}
-          <div className="flex justify-start lg:justify-start">
+          <div className="flex justify-start lg:absolute lg:left-0 lg:-ml-12">
             <Image
               src="/images/saltbox-logo.svg"
               alt="Saltbox Interactive"
@@ -20,15 +20,15 @@ export default function EmailSubscribe() {
           </div>
 
           {/* Center - Heading and Subscribe Button */}
-          <div className="text-center space-y-6 sm:space-y-8">
+          <div className="flex-1 text-left space-y-6 sm:space-y-8 lg:mx-auto lg:max-w-xl lg:pt-0">
             <h2
-              className="text-3xl sm:text-4xl md:text-5xl font-light tracking-[0.2em] text-white"
+              className="text-2xl sm:text-3xl md:text-4xl font-light tracking-[0.2em] text-white"
               style={{ fontFamily: 'var(--font-bebas)' }}
             >
-              SIGN UP TO OUR EMAIL
+              SUBSCRIBE TO OUR EMAIL TO DISCOVER WITH US.
             </h2>
 
-            <div className="flex justify-center">
+            <div className="flex justify-start">
               <BracketLink href="/contact">
                 SUBSCRIBE
               </BracketLink>
@@ -36,7 +36,7 @@ export default function EmailSubscribe() {
           </div>
 
           {/* Right - Image */}
-          <div className="relative w-full aspect-[4/3] lg:aspect-[3/2]">
+          <div className="relative w-full aspect-[4/3] lg:aspect-[3/2] lg:w-1/3 lg:absolute lg:right-0 lg:mr-[-6rem]">
             <Image
               src="/images/temp/dod-temp-12.jpg"
               alt="Historic site"
