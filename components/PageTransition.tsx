bbef19117f24532c4cff10b39b2669434090d961
michaelsalton <msalton6@gmail.com>
2025-12-11 22:52:53 -0500

    ts error fixed

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -15,7 +15,7 @@ const pageVariants = {
     scale: 1,
     transition: {
       duration: 0.5,
-      ease: [0.22, 1, 0.36, 1], // Custom easing for smooth feel
+      ease: [0.22, 1, 0.36, 1] as const, // Custom easing for smooth feel
       staggerChildren: 0.1,
     },
   },
